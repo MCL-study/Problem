ab0da6f1b770048eb045488f67a1d56c184c12a9
sylphe <myks790@gmail.com>
2016-04-27 11:35:27 +0900

    add Test User2

---
+++ user2/problem001/Solution_david.java
@@ -0,0 +1,14 @@
+public class Solution_david {
+
+    public static void main(String[] args){
+        int a=100;
+        int b=50;
+        System.out.println("A : "+a+" B : "+b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("A : "+a+" B : "+b);
+    }
+}

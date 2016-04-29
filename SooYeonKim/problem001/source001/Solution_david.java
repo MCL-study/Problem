28b79dfcd0a364aadd3690a784c1ec96b3ab4464
sylphe <myks790@gmail.com>
2016-04-29 09:50:09 +0900

    SooYeonKim 추가

---
+++ SooYeonKim/problem001/source001/Solution_david.java
@@ -0,0 +1,16 @@
+package source001;
+
+public class Solution_david {
+
+    public static void main(String[] args){
+        int a=100;
+        int b=50;
+        System.out.println("A : "+a+" B : "+b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("A : "+a+" B : "+b);
+    }
+}

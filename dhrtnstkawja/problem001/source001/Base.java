072d66e02c9997c9f3d88adf0e1f2409fc6023e7
sylphe <myks790@gmail.com>
2016-04-29 09:35:58 +0900

    dhrtnatkawja 추가

---
+++ dhrtnstkawja/problem001/source001/Base.java
@@ -0,0 +1,16 @@
+package source001;
+
+/*수정 하지 마시오*/
+/* 자기 이름으로 하위 디렉토리를 만들어서 답을 작성하시오. */
+class Base{
+
+    public static void main(String[] argv){
+        int a=100;
+        int b=50;
+        System.out.println("A : "+a+" B : "+b);
+
+        //to do
+
+        System.out.println("A : "+a+" B : "+b);
+    }
+}

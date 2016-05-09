c5c95b550e1d72e18658af83fb4649a7a4af3eed
sylphe <myks790@gmail.com>
2016-05-09 10:21:39 +0900

    문제 3추가

---
+++ ksh/problem003/redeme.md
@@ -0,0 +1,14 @@
+windows에서만 동작하는 예제입니다.
+
+base.cpp에서   "to do" 부분만 수정하여
+매트릭스를 완성하시오!
+
+참고 자료는 아래 url 하나만 보도록 합시다.
+(http://soen.kr/lecture/ccpp/cpp2/14-1-3.htm)
+
+머리가 너무 복잡하다면 hint.md를 보세요.
+//글자들이 떨어져 내립니다.
+![예제_사진](./img/img.PNG)
+
+(http://soen.kr)
+위 사이트의 C/C++ 강좌 -> 2부 C고급 -> 14-1.Matrix 에 매트릭스 관련 자료가 다 있습니다.
---
+++ ksh/problem003/source003/base.cpp
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <conio.h>
+#define MAX 1024
+#define LEFT 75
+#define RIGHT 77
+#define UP 72
+#define DOWN 80
+#define ESC 27
+
+void gotoxy(int x, int y);
+struct Signal
+{
+	BOOL exist;                // 신호의 존재 여부
+	char ch;                 // 출력할 문자
+	int x, y;                   // 현재 좌표
+	int distance;           // 이동할 거리
+	int nFrame;                 // 속도
+	int nStay;               // 속도에 대한 카운트
+};
+struct Signal S[MAX];
+void main()
+{
+	int i;
+	int ch;
+	int freq = 15;
+	int frame = 200;
+
+	gotoxy(1, 23);
+	printf("매트릭스 흉내내기. 상하:속도 증감, 좌우:빈도 증감, ESC:종료");
+
+	for (;;) {
+		gotoxy(60, 23);
+		printf("속도:%d, 빈도:%d    ", frame, freq);
+
+		// 키 입력 처리
+		if (kbhit()) {
+			ch = getch();
+			if (ch == 0xE0) {
+				ch = getch();
+				switch (ch) {
+				case UP:
+					frame = min(frame + 10, 1000);
+					break;
+				case DOWN:
+					frame = max(frame - 10, 10);
+					break;
+				case RIGHT:
+					freq = max(freq - 1, 1);
+					break;
+				case LEFT:
+					freq = min(freq + 1, 30);
+					break;
+				}
+			}
+			else {
+				if (ch == ESC) {
+					return;
+				}
+			}
+		}
+
+		// 새로운 신호 생성
+		if (rand() % (freq + 1) == 0)
+		{
+			for (i = 0; i < MAX; i++) {
+				if (S[i].exist == false) {
+					//to do  start*******************************
+
+                       //이 곳만 수정 하시오.
+                       //다른 곳은 수정 하지 마시오
+
+		            //to do end*******************************
+					break;
+				}
+			}
+			// 신호 이동 및 주기가 다 된 신호 제거 처리
+			for (i = 0; i < MAX; i++) {
+				if (S[i].exist == FALSE)
+					continue;
+				if (--S[i].nStay == 0) {
+					S[i].nStay = S[i].nFrame;
+					gotoxy(S[i].x, S[i].y); putch(' ');
+					if (++S[i].y < S[i].distance) {
+						gotoxy(S[i].x, S[i].y); putch(S[i].ch);
+					}
+					else {
+						S[i].exist = FALSE;
+					}
+				}
+			}
+			Sleep(1000 / frame);
+		}
+	}
+}
+void gotoxy(int x, int y)
+{
+	COORD Pos = { x - 1, y - 1 };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
